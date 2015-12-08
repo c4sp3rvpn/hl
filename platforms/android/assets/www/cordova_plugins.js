0cdb188074f71849f7a2aa25672d61ee09d99d03
c4sp3rvpn <v.kozlov@rublevski.by>
2015-12-08 18:54:29 +0300

    li

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-yandexnavi/www/yandexnavi.js",
+        "id": "cordova-plugin-yandexnavi.Yandexnavi",
+        "clobbers": [
+            "Yandexnavi"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-yandexnavi": "0.0.0"
+}
+// BOTTOM OF METADATA
+});
